10d0b43dae606368f230529adc5317cb2e52a5b3
Vege19 <josue.ardon113@gmail.com>
2019-04-12 18:55:14 -0600

    Bottom navigation view implemented

---
+++ app/src/main/java/github/vege19/popnow/Fragments/TVShowsFragment.java
@@ -0,0 +1,22 @@
+package github.vege19.popnow.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import github.vege19.popnow.R;
+
+public class TVShowsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tvshows, container, false);
+
+    }
+
+}
--- app/src/main/java/github/vege19/popnow/MainActivity.java
+++ app/src/main/java/github/vege19/popnow/MainActivity.java
@@ -1,13 +1,73 @@
 package github.vege19.popnow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import github.vege19.popnow.Fragments.MoviesFragment;
+import github.vege19.popnow.Fragments.TVShowsFragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.navigationView);
+
+        //Init base navigation
+        initContent();
+
     }
+
+    //Init fragments and bottom navigation view functionality
+    private void initContent() {
+
+        //Fragments
+        final Fragment moviesFragment = new MoviesFragment();
+        final Fragment tvshowsFragment = new TVShowsFragment();
+
+        fragmentManager = getSupportFragmentManager();
+
+        //Setting movie fragment as first to load
+        fragmentManager.beginTransaction()
+                .replace(R.id.navigationContainer, moviesFragment)
+                .commit();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                Fragment currentFragment = null;
+
+                //Switch to select fragments
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_movies:
+                        currentFragment = moviesFragment;
+                        break;
+                    case R.id.navigation_tv:
+                        currentFragment = tvshowsFragment;
+                        break;
+                }
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.navigationContainer, currentFragment)
+                        .commit();
+
+                return true;
+            }
+
+        });
+
+    }
+
 }
